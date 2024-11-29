75bed1c1c47155b5ef484dbd78241fb27a60fb2b
Timur <109920371+powerpless@users.noreply.github.com>
2024-11-29 20:53:27 +0500

    Add files via upload

---
+++ hello.js
@@ -0,0 +1,11 @@
+(function (window) {
+    const helloSpeaker = {};
+    const speakWord = "Hello";
+  
+    helloSpeaker.speak = function (name) {
+      console.log(speakWord + " " + name);
+    };
+  
+    window.helloSpeaker = helloSpeaker;
+  })(window);
+  
---
+++ script.js
@@ -0,0 +1,14 @@
+(function () {
+    const names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+  
+    for (let i = 0; i < names.length; i++) {
+      const firstLetter = names[i].charAt(0).toLowerCase();
+  
+      if (firstLetter === "j") {
+        goodbyeSpeaker.speak(names[i]);
+      } else {
+        helloSpeaker.speak(names[i]);
+      }
+    }
+  })();
+  
